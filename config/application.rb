5282600c2f5daaf85e3cf83ac98220f50c5e703a
Jakson Rochelly <jrochelly@gmail.com>
2013-08-01 17:15:42 -0300

    Open Sans font is now local

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module Sip
   class Application < Rails::Application
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
